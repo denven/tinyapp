aa442b16ebab017966caccb86551b0cd9ccf4fd7
denven <myregs@foxmail.com>
2019-10-28 17:58:32 -0700

    add random string generator func

--- express_server.js
+++ express_server.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+const generateRandomString = function(len) {
+
+  let retStrRandom = "";
+  const strTemplate = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < len; i++) {
+    let index = Math.ceil(Math.random() * 100000000) % strTemplate.length;
+    retStrRandom += strTemplate.charAt(index);
+  }
+  return retStrRandom;
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let strRandom = generateRandomString(6);
+  res.send(strRandom);         // Respond with 'Ok' (we will replace this)
+});
 
 app.set("view engine", "ejs");
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +49,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.url };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!' };
