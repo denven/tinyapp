7e0f01cde725e51a08f3c4a864196a6bb6704ce0
denven <myregs@foxmail.com>
2019-10-28 22:37:06 -0700

    complete code refactor

--- express_server.js
+++ express_server.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
+//generate randomly of len chars' string
 const generateRandomString = function(len) {
-
   let retStrRandom = "";
-  const strTemplate = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const strTemplate = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789";
   for (let i = 0; i < len; i++) {
     let index = Math.ceil(Math.random() * 100000000) % strTemplate.length;
     retStrRandom += strTemplate.charAt(index);
@@ -14,61 +14,60 @@ const generateRandomString = function(len) {
   return retStrRandom;
 };
 
+//bodyParser will translate body into req.body for forms POST
 app.use(bodyParser.urlencoded({extended: true}));
+
+//POST should put before GET methond
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
   let strRandom = generateRandomString(6);
-  res.send(strRandom);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[strRandom] = req.body.longURL;
+  res.redirect(`/urls/${strRandom}`);
 });
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  ":u7Po9": "http://www.facebook.com"
 };
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Welcome to TinyUrl App!");
 });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.url };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  //console.log(templateVars);
   res.render("urls_show", templateVars);
 });
 
-
-app.get("/hello", (req, res) => {
-  let templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
+app.get("/u/:shortURL", (req, res) => {
+  //console.log("Now in /u/:shortURL, with req.url:",  req.url);
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  //console.log("two urls", shortURL, longURL);
+  res.redirect(longURL);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  //res.send(`a = ${a}`);
+//for testing
+app.get("/jeremy/:foo/:bar/:baz", (req, res) => {
+  console.log("req params", req.params);
+  res.json(req.params);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyURL app listening on port ${PORT}!`);
 });
